c1a6b7cdb80604ac8eb9f59f019dbf6a88d97e33
Kritika049 <94595813+Kritika049@users.noreply.github.com>
2021-12-08 15:15:25 +0530

    Delete user_controller.py

--- user_controller.py
+++
@@ -1,5 +0,0 @@
-class UserController(object):
-    def __init__(self,userService):
-        self.userService = userService
-    def addUser(self,id,name):
-        return self.userService.addUser(id,name)
