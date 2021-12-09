abfd6015c3c1517b1222dcdad7aaa948d4c09dfc
Kritika049 <94595813+Kritika049@users.noreply.github.com>
2021-12-09 17:45:57 +0530

    Delete Music_Recommender directory

--- Music_Recommender/playlist_service_interface.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-
-class playlistServiceInterface(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def addPlaylist(self, id, userId, name, songs):
-        pass
-
--- Music_Recommender/user_controller.py
+++
@@ -1,6 +0,0 @@
-class userController(object):
-    def __init__(self, userService):
-        self.userService = userService
-
-    def addUser(self, id, name):
-        return self.userService.addUser(id, name)
